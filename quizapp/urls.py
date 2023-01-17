f7785a725dda718a8adaa81fa58dab0c566f4e44
binayuchai <binayuchai58@gmail.com>
2023-01-17 19:13:57 +0545

    Important changes after revert

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -4,5 +4,5 @@ from quizapp.views import home,result
 app_name = "quizapp"
 urlpatterns = [
     path("",home,name="home"),
-    path("submit-result/",result,name="result"),
+    path("result/",result,name="result")
 ]
